61d8d06599178043f9c60122e744cec39366d437
Marcos Willian <markimwrs@hotmail.com>
2021-07-05 19:51:04 -0300

    Resolveu o BUG

--- Simple_talk_Client.c
+++ Simple_talk_Client.c
@@ -12,39 +12,55 @@
 #define MAX_LINE 256
 
 int main(int argc, char * argv[]){
-    FILE *fp;
+    printf("Inicializando client...\n");
+    /**======================
+     **      Variáveis
+     *========================**/
     struct sockaddr_in server;
     char *host;
     char buf[MAX_LINE];
     int sock;
     int len;
 
+    /**======================
+     **      Inicializa server
+     *========================**/
     if( argc == 2) {
         host = argv[1];
     }else{
-        fprintf(stderr, "usage: Simple_talk_Client IP_server\n");
+        fprintf(stderr, "Usage: Simple_talk_Client IP_server\n");
         exit(1);
     }
-    
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY/*inet_addr(host)*/;
+    server.sin_addr.s_addr = inet_addr(host);
     server.sin_port = htons(SERVER_PORT);
     
-    if((sock = socket(AF_INET, SOCK_STREAM, 0) < 0)){
-        perror("Erro na criação do socket!");
+    /**======================
+     **      Realiza a conexão
+     *========================**/
+    if((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror("Erro na criação do socket:");
         exit(1);
     }
 
+    printf("Conectando ao servidor: %s:%d\n", host, SERVER_PORT);
     if(connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0){
-        perror("Erro na conecção!");
+        perror("Erro na conexão:");
         close(sock);
         exit(1);
     }
+    printf("Conectado ao servidor: %s:%d com sucesso\n", host, SERVER_PORT);
 
+    /**======================
+     **      Troca de mensagem
+     *========================**/
+    printf("\nEscreva uma mensagem:\n");
     while (fgets(buf, sizeof(buf), stdin)){
         buf[MAX_LINE - 1] = '\0';
         len = strlen(buf) + 1;
         send(sock, buf, len, 0);
+        printf("Mensagem enviada com sucesso!\n");
+        printf("\nEscreva uma mensagem:\n");
     }
 }
--- Simple_talk_Server.c
+++ Simple_talk_Server.c
@@ -9,42 +9,56 @@
 #include <netdb.h>
 
 #define SERVER_PORT 54321
+#define SERVER_IP INADDR_ANY
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
 int main(){
+    printf("Inicializando servidor...\n");
+    /**======================
+     **      Variáveis
+     *========================**/
     struct sockaddr_in server;
     char buf[MAX_LINE];
     int len;
     int sock, client_sock;
 
+    /**======================
+     **      Inicializa servidor
+     *========================**/
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_addr.s_addr = SERVER_IP; 
     server.sin_port = htons(SERVER_PORT);
 
     if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        perror("Erro na criação do socket!");
+        perror("Erro na criação do socket:");
         exit(1);
     }
     if((bind(sock, (struct sockaddr *) &server, sizeof(server))) < 0){
-        perror("Erro no bind!");
+        perror("Erro no bind:");
         exit(1);  
     }
-    printf("ouvindo\n");
+    printf("Servidor: %s:%d  inicializado com sucesso\n",  inet_ntoa(server.sin_addr), SERVER_PORT);
+    /**======================
+     **      Interação com o client
+     *========================**/
     listen(sock, MAX_PENDING);
-    printf("lendo\n");
     len = sizeof(server);
     while(1){
+        printf("Aguardando conexão do client\n");
         if((client_sock = accept(sock, (struct sockaddr *) &server, &len)) < 0){
-            perror("Erro na conecção!");
+            perror("Erro na conexão com o client:");
             exit(1); 
         }
-        printf("ish\n");
+        printf("Client conectado\n");
         while(len = recv(client_sock, buf, sizeof(buf), 0)){
+            printf("Cliente:\t");
             fputs(buf, stdout);
         }
+        printf("Client desconectado\n");
         close(client_sock);
     }
+    close(sock);
 
 }
